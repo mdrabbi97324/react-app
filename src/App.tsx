9ed17d9f8b75e84efa1c6f6af41a6f3b997607ce
mdrabbi97324 <mdrabbi97324@gmail.com>
2024-03-18 15:20:29 +0600

    connecting to the backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,105 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import axios, { AxiosError, CanceledError } from "axios";
 
-function App() {
-  const [expenses, setExpenses] = useState([
-    {
-      id: 1,
-      description: "Milk",
-      amount: 10,
-      category: "Groceries",
-    },
-
-    {
-      id: 2,
-      description: "Coffee",
-      amount: 10,
-      category: "Groceries",
-    },
-
-    {
-      id: 3,
-      description: "Mouse",
-      amount: 10,
-      category: "Utilities",
-    },
-
-    {
-      id: 4,
-      description: "Keyboard",
-      amount: 10,
-      category: "Utilities",
-    },
-  ]);
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((expense) => expense.category === selectedCategory)
-    : expenses;
-  return (
-    <div>
-      <div className="mb-3">
-        <ExpenseForm
-          onSubmit={(newExpense) =>
-            setExpenses([
-              ...expenses,
-              { ...newExpense, id: expenses.length + 1 },
-            ])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) =>
-          setExpenses(expenses.filter((expense) => expense.id !== id))
-        }
-      />
-    </div>
-  );
+interface User {
+  id: number;
+  name: string;
 }
 
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const controller = new AbortController();
+    setIsLoading(true);
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setError("");
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setUsers([]);
+        setIsLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  const deleteUser = (user: User) => {
+    setUsers(users.filter((u) => u.id !== user.id));
+    const originalUsers = [...users];
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/users/` + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: "Golam Rabbi" };
+    setUsers([newUser, ...users]);
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + " !!!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+    axios.patch(`https://jsonplaceholder.typicode.com/users/` + user.id, updateUser)
+    .catch(err => {
+      setError(err.message);
+      setUsers(originalUsers)
+    })
+  };
+
+  return (
+    <>
+      {isLoading && <div className="spinner-border"></div>}
+      {error && <p className="text-danger">{error}</p>}
+      <button className="btn btn-primary mb-3" onClick={() => addUser()}>
+        Add
+      </button>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-2"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
 export default App;
